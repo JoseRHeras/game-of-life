4055c0d9cab5e51f57af664a50e29da42e7a9506
Jose Heras <jrheras@yahoo.com>
2022-01-31 19:38:59 -0600

    ControlBar class updated. Reset button added it along with login implementation on the activation or deactivation of either button depending on the actual state of the program

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -65,6 +65,9 @@ class GameOfLife:
 
         self.table = new_stage_table
 
+    def discard_table_and_generate_new_one(self) -> None:
+        self._populate_initial_table()
+
     def is_cell_alive(self, row: int, col: int) -> bool:
         return True if self.table[row, col] == 1 else False
 
@@ -86,7 +89,7 @@ class CellContainer:
         canvas_size = self.game_of_life.size * self.cell_size
 
         self.canvas = tk.Canvas(master=self.master, width=canvas_size, height=canvas_size, bg="white")
-        self.canvas.pack(padx=5, pady=5)
+        self.canvas.pack()
 
     def _populate_container(self) -> None:
         self.canvas.delete("all")
@@ -104,6 +107,10 @@ class CellContainer:
 
             coordinate_y1 += self.cell_size
             coordinate_y2 += self.cell_size
+
+    def reset_container(self) -> None:
+        self.game_of_life.discard_table_and_generate_new_one()
+        self._populate_container()
 
     def evolve_and_update_container(self) -> None:
         self.game_of_life.mutate_table_to_next_stage()
--- src/visualizer.py
+++ src/visualizer.py
@@ -1,27 +1,30 @@
+from cgitb import text
+from doctest import master
 import tkinter as tk
 from src.game_of_life import CellContainer
 
 
 class Game:
-
     def __init__(self) -> None:
         self._initialize_graphical_window()
         self._build_widgets()
 
     def _initialize_graphical_window(self) -> None:
         self.root: tk.Tk = tk.Tk()
-        self.root.title = "Conway Game Of Life"
+        self.root.title(string="Conway's Game Of Life")
+        self.root.config( padx=10, pady=5 )
 
     def _build_widgets(self) -> None:
         self.cell_container: CellContainer = CellContainer(master=self.root)
-        self.control_bar: ControlBar = ControlBar(master=self.root, target=self.cell_container)
+        self.control_bar: ControlBar = ControlBar(
+            master=self.root, target=self.cell_container
+        )
 
     def run(self) -> None:
         self.root.mainloop()
 
 
 class ControlBar:
-
     def __init__(self, master: tk.Tk, target: CellContainer) -> None:
         self.master: tk.Tk = master
         self.target: CellContainer = target
@@ -30,23 +33,48 @@ class ControlBar:
     def _build_graphical_component(self) -> None:
         self.component_frame = tk.Frame(master=self.master)
 
-        start_button = tk.Button(master=self.component_frame,
-                                 text="Start Animation",
-                                 command=self._start_animation)
-        stop_button = tk.Button(master=self.component_frame,
-                                text="Stop Animation",
-                                command=self._stop_animation)
+        self.start_button = tk.Button(
+            master=self.component_frame,
+            text="Start Animation",
+            command=self._start_animation,
+        )
+        self.stop_button = tk.Button(
+            master=self.component_frame,
+            text="Stop Animation",
+            command=self._stop_animation,
+            state=tk.DISABLED
+        )
 
-        start_button.grid(row=1, column=1, padx=3, pady=2)
-        stop_button.grid(row=1, column=2, padx=3, pady=2)
-        self.component_frame.pack(fill=tk.BOTH, expand=True)
-        
+        self.reset_button = tk.Button(
+            master=self.component_frame,
+            text="Restart Animation",
+            command=self._reset,
+            state=tk.DISABLED
+        )
+
+        self.start_button.grid(row=1, column=1, padx=3)
+        self.stop_button.grid(row=1, column=2, padx=3)
+        self.reset_button.grid(row=1, column=3, padx=3)
+        self.component_frame.pack(fill=tk.BOTH, expand=True, pady=7)
 
     def _start_animation(self) -> None:
+
+        self.start_button.config(state=tk.DISABLED)
+        self.stop_button.config(state=tk.NORMAL)
+        self.reset_button.config(state=tk.DISABLED)
+
         self.target.evolve_and_update_container()
         self.animation_id = self.component_frame.after(1500, self._start_animation)
 
     def _stop_animation(self) -> None:
+        
+        self.stop_button.config(state=tk.DISABLED)
+        self.start_button.config(state=tk.NORMAL)
+        self.reset_button.config(state=tk.NORMAL)
+
         self.component_frame.after_cancel(self.animation_id)
 
-
+    def _reset(self) -> None:
+        self.reset_button.config(state=tk.DISABLED)
+        self.start_button.config(state=tk.NORMAL)
+        self.target.reset_container()
